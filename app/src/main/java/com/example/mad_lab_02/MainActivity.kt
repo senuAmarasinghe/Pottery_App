14ef6e8a18d80cc2b499b1a5c51dc9d49fe91fcf
Senuthii Amarasinghe <asenuthisahansa@gmail.com>
2025-04-21 19:01:06 +0530

    Unversioned files

---
+++ app/src/main/java/com/example/mad_lab_02/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mad_lab_02
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish() // Close splash screen so it doesn’t stay in back stack
+        }, 3000) // 2000ms = 2 second
+
+
+
+
+    }
+}
